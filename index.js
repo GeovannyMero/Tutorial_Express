b70fd4d9d16876a42fd90cba4cbaea1ab8cd9742
Geovanny Mero <jefferson_mero1911@hotmail.com>
2021-05-14 00:21:15 -0500

    route with parameters

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var express = require("express"); //importamos a express
 var app = express(); //se llama a express
 
 //asignamos una ruta
-app.get("/", function(req, res){
-    res.send("Hello word");
+app.get("/", function (req, res) {
+  res.send("Hello word");
 });
 
+//ruta con un parametro
+app.get("/:name", function (req, res) {
+  res.send(`Your name is ${req.params.name}`);
+});
+
+//ruta con dos parametros
+app.get("/:name/:lastname", function (req, res) {
+  res.send(`Your full name is ${req.params.name} ${req.params.lastname}`);
+});
 //asignamos el puerto
 app.listen(3000);
-
-
-
