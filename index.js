740b77e9f072fa86426683244be4a4846154fc25
Geovanny Mero <jefferson_mero1911@hotmail.com>
2021-05-14 00:12:17 -0500

    first route

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var express = require("express");
+var express = require("express"); //importamos a express
+var app = express(); //se llama a express
+
+//asignamos una ruta
+app.get("/", function(req, res){
+    res.send("Hello word");
+});
+
+//asignamos el puerto
+app.listen(3000);
+
+
+
